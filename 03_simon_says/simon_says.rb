3f444154394222594a156749a57535bff00b94c0
Christopher Dale <zecheeseking@gmail.com>
2019-09-21 00:24:57 +0200

    added in rest of the functions.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -10,3 +10,20 @@ end
 def repeat(word, repetitions = 2)
   ("#{word} " * repetitions)[0...-1]
 end
+
+def start_of_word(word, pos)
+  word[0...pos]
+end
+
+def first_word(sentence)
+  sentence.partition(" ").first
+end
+
+def titleize(words)
+  smallwords = ["the", "and", "over"]
+  capitalizedWords = words.split(" ").map do |word|
+    smallwords.include?(word)? word : word.capitalize
+  end
+  capitalizedWords.first.capitalize!
+  capitalizedWords.join(" ")
+end
