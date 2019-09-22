645b2364f70023c99adfd30946c0755345dea4df
Christopher Dale <zecheeseking@gmail.com>
2019-09-22 15:15:34 +0200

    finished pig latin translator function

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,11 +1,14 @@
 #write your code here
-def translate(word)
+def translate(original_text)
   vowels = ['a', 'e', 'i', 'o', 'u']
-
-  if vowels.include?(word[0])
-    return word + 'ay'
-  else
-    vowel_index = word.index(/[aeiou]/)
-    return word[vowel_index...word.length] + word[0...vowel_index] + 'ay'
+  words = original_text.split(" ").map do |word|
+    if word[0].match(/[aeiou]/)
+      word + 'ay'
+    else
+      vowel_index = word.index(/(?<!q)[aeiou]/)
+      word[vowel_index...word.length] + word[0...vowel_index] + 'ay'
+    end
   end
+
+  words.join(" ")
 end
