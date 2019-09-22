1e1f2d515fcb3ae4e534b459a2463f40500927f6
Christopher Dale <zecheeseking@gmail.com>
2019-09-22 15:55:37 +0200

    Added in timer function.

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,13 @@
 class Book
 # write your code here
+  def title=(new_title)
+    unimportant_words = ["and", "to", "the", "an", "of", "in", "a"]
+    @title = new_title.split(" ").map { |word| unimportant_words.include?(word)? word : word.capitalize }
+    @title.first.capitalize!
+    @title = @title.join(" ")
+  end
+
+  def title
+    @title
+  end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,22 @@
 class Timer
   #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds=(new_seconds)
+    @seconds = new_seconds
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def time_string
+    secs = @seconds % 60
+    minutes = @seconds / 60
+    hours = minutes / 60
+    minutes = minutes % 60
+    "#{hours < 10 ? "0#{hours}" : hours}:#{minutes < 10 ? "0#{minutes}" : minutes }:#{secs < 10 ? "0#{secs}" : secs}"
+  end
 end
