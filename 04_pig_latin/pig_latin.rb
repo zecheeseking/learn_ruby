8fc49c2e5447cffdea6f6a0a8a88de2405996cd5
Christopher Dale <zecheeseking@gmail.com>
2019-09-22 14:03:23 +0200

    Initial version of file working with single vowel/consonant

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,10 @@
 #write your code here
+def translate(word)
+  vowels = ['a', 'e', 'i', 'o', 'u']
+
+  if vowels.include?(word[0])
+    return word + 'ay'
+  else
+    return word[1...word.length] + word[0] + 'ay'
+  end
+end
